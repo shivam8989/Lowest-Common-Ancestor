93cf45f03da58848e50848a12387db1988516ac1
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-02-23 16:03:55 +0530

    Add files via upload
    The lowest common ancestor (LCA) of two nodes in a binary tree is the deepest node which is the ancestor of both the nodes.

---
+++ Lowest_Common_Ancestor.java
@@ -0,0 +1,60 @@
+package Binary_Tree;
+import java.util.ArrayList;
+public class Lowest_Common_Ancestor {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean getpath(Node root, int n, ArrayList<Node> path){
+        if(root==null){
+            return false;
+        }
+        path.add(root);
+        if(root.data == n){
+            return true;
+        }
+        boolean foundleft = getpath(root.left, n , path);
+        boolean foundright = getpath(root.right, n, path);
+
+        if(foundleft || foundright){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+        getpath(root,n1,path1);
+        getpath(root,n2,path2);
+
+        int i = 0;
+        for(; i<path1.size() && i<path2.size();  i++){
+            if (path1.get(i) != path2.get(i)) {
+
+                break;
+            }
+        }
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+    public static void main(String args []){
+            Node root = new Node(1);
+            root.left = new Node(2);
+            root.right = new Node(3);
+            root.left.left = new Node(4);
+            root.left.right = new Node(5);
+            root.right.right = new Node(6);
+            System.out.println(lca(root,4,6).data);
+    }
+
+
+}
